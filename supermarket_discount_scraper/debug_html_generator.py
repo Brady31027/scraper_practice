2b464c5fea83362addf550aeb3f8387556c1b8e5
Brady Cheng <brady31027@gmail.com>
2017-02-15 11:46:54 -0800

    add coupon page html generation function

--- supermarket_discount_scraper/debug_html_generator.py
+++ supermarket_discount_scraper/debug_html_generator.py
@@ -1,12 +1,22 @@
 import time
 from selenium import webdriver
 
-url = 'http://www.wholefoodsmarket.com/sales-flyer/cupertino'
+CONFIG_ON_SALE = 0
+
+if CONFIG_ON_SALE == 1:  # get on sale page html source
+    url = 'http://www.wholefoodsmarket.com/sales-flyer/cupertino'
+else:
+    url = 'http://www.wholefoodsmarket.com/sales-flyer/cupertino?qt-custom_quicktab_sales_coupons=1#qt-custom_quicktab_sales_coupons'
+
 cmd = 'return document.documentElement.outerHTML'
 driver = webdriver.Chrome(executable_path='./bin/chromedriver')
 driver.get(url)
 time.sleep(5)
 html = driver.execute_script(cmd)
 
-with open("whold_food_debug_html.txt", 'w+') as fout:
-    fout.write(html)
+if CONFIG_ON_SALE == 1:
+    with open("whold_food_debug_html.txt", 'w+') as fout:
+        fout.write(html)
+else:
+    with open("whold_food_coupon_debug_html.txt", 'w+') as fout:
+        fout.write(html)
