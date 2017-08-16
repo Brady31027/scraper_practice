e041f7b7442600736160783c86a68e5e027f6932
Brady Cheng <brady31027@gmail.com>
2017-08-16 02:00:24 -0700

    integrate frontend form to django backend

--- cowBaySilliconValley/django/cowbaySV/cowbay/views.py
+++ cowBaySilliconValley/django/cowbaySV/cowbay/views.py
@@ -1,8 +1,25 @@
 # -*- coding: UTF-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django import forms
+from django.views.decorators.csrf import ensure_csrf_cookie
 
-# Create your views here.
+@ensure_csrf_cookie
 def index(request):
-	return render_to_response('index.html')
+	contents = ""
+	valid = -1
+	if request.method == 'POST':
+		try:
+			contents = request.POST['contents']
+			if len(contents.strip()) == 0:
+				contents = ""
+				valid = 0
+				pass
+			else:
+				valid = 1
+		except:
+			contents = ""
+			valid = 0
+			pass
+		
+	return render(request, 'index.html', locals())
--- cowBaySilliconValley/django/cowbaySV/static/js/ui.js
+++ cowBaySilliconValley/django/cowbaySV/static/js/ui.js
@@ -4,20 +4,28 @@ function onMouseOverTab(selected) {
 function onMouseOutTab(selected) {
 	selected.style.background = '#f7f7f7';
 }
-function toast() {
+function toast_early_check() {
     // Get the snackbar DIV
-    var suc = document.getElementById('snackbarSuc');
     var err = document.getElementById('snackbarErr');
     var contents = document.getElementById('contents').value.trim();
-    var ui;
     if (contents.length > 0) {
-    	ui = suc;
-    } else {
-    	ui = err;
+        err.className = "show";
+        setTimeout(function(){ err.className = err.className.replace("show", ""); }, 3000);
     }
+}
 
-    ui.className = "show";
-    // After 3 seconds, remove the show class from DIV
-    setTimeout(function(){ ui.className = ui.className.replace("show", ""); }, 3000);
+function toast_err() {
+    // Get the snackbar DIV
+    var err = document.getElementById('snackbarErr');
+    err.className = "show";
+    setTimeout(function(){ err.className = err.className.replace("show", ""); }, 3000);
+    
+}
+
+function toast_suc() {
+    // Get the snackbar DIV
+    var suc = document.getElementById('snackbarSuc');
+    suc.className = "show";
+    setTimeout(function(){ suc.className = suc.className.replace("show", ""); }, 3000);
     
 }
