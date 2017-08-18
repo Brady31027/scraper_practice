dc1480e88a13a8f743d03ae1b7c2b927f460128c
Brady Cheng <brady31027@gmail.com>
2017-08-18 14:57:42 -0700

    add heroku config

--- cowBaySilliconValley/django/cowbaySV/cowbaySV/settings.py
+++ cowBaySilliconValley/django/cowbaySV/cowbaySV/settings.py
@@ -11,9 +11,21 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
+
+onHeroku = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = ''
+DATABASES = ''
+STATIC_ROOT = ''
+STATICFILES_STORAGE = ''
+
+if onHeroku:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+else:
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -72,14 +84,18 @@ WSGI_APPLICATION = 'cowbaySV.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if onHeroku:
+    DATABASES['default'] =  dj_database_url.config()
+    DATABASES['default']['CONN_MAX_AGE'] = 500
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    ALLOWED_HOSTS = ['*']
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -116,6 +132,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+if onHeroku:
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
--- cowBaySilliconValley/django/cowbaySV/cowbaySV/wsgi.py
+++ cowBaySilliconValley/django/cowbaySV/cowbaySV/wsgi.py
@@ -10,7 +10,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise #heroku add
+
+onHeroku = False
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cowbaySV.settings")
 
-application = get_wsgi_application()
+application = ''
+if onHeroku:
+	application = DjangoWhiteNoise(application)
+else:
+	application = get_wsgi_application()
+
+
