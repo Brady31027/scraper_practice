d715fa3efac884d9b3a89ca8f40842c5ecdb2618
Brady Cheng <brady31027@gmail.com>
2017-08-18 17:25:03 -0700

    sync to the latest

---
+++ cowBaySilliconValley/dev/facebook.py
@@ -0,0 +1,24 @@
+#粉絲專頁編號 958320734245072
+#應用程式編號 358038677963388
+#API 版本 API 版本
+#應用程式密鑰 66aa0aeedf35c7f33941129eea6a385f
+"""
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '358038677963388',
+      xfbml      : true,
+      version    : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+"""
